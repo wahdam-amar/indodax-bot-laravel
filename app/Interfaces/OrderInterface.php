9460e91c6ca848262328e08a4cfcec436447848b
wahdam <wahdam.amarulloh@gmail.com>
2021-11-06 12:47:51 +0700

    fix make order

--- app/Interfaces/OrderInterface.php
+++ app/Interfaces/OrderInterface.php
@@ -5,7 +5,8 @@ namespace App\Interfaces;
 interface OrderInterface
 {
     public function openOrders();
+    public function hasOrders(String $coin, String $type = 'buy');
     public function sucessfulOrders();
     public function putOrder();
-    public function cancelOrder($orderId);
+    public function cancelOrder($pair, $order_id, $type = 'buy');
 }
