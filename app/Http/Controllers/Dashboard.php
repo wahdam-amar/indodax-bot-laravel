23741af4ef752ea257023bcfeac90f483e652e6b
wahdam <wahdam.amarulloh@gmail.com>
2021-11-06 16:23:52 +0700

    clear dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -10,16 +10,6 @@ class Dashboard extends Controller
 {
     public function main()
     {
-        $account = new LiveAccount(auth()->id());
-
-        $order = $account->putOrder('eth', '500000', '500000');
-
-        if ($order) {
-            debug($order);
-        } else {
-            debug('error');
-        }
-
         $openOrders = indodax()->openOrders();
 
         return view('asset')->with('orders', $openOrders);
