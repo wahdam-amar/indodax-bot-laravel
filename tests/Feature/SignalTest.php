6ada440ecc269bbb22e745bb0db5885bc589ac13
wahdam <wahdam.amarulloh@gmail.com>
2021-12-24 12:56:59 +0700

    Create signal test

--- tests/Feature/SignalTest.php
+++ tests/Feature/SignalTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Signals\Rsi;
 use Illuminate\Pipeline\Pipeline;
+
+use function PHPUnit\Framework\assertFalse;
 use function PHPUnit\Framework\assertTrue;
 
 class SignalTest extends TestCase
@@ -30,6 +32,29 @@ class SignalTest extends TestCase
             });
 
         assertTrue($pipe->get('should_buy'));
+    }
+
+    /** @test */
+    public function cant_signal_buy_with_rsi()
+    {
+        $indicators = collect([
+            0 => (object)[
+                'id' => 'rsi',
+                'result' => (object) ['value' => 31]
+            ]
+        ]);
+
+        $pipe = app(Pipeline::class)
+            ->send($indicators)
+            ->via('buy')
+            ->through([
+                Rsi::class
+            ])
+            ->then(function ($indicators) {
+                return $indicators;
+            });
+
+        assertFalse($pipe->get('should_buy', false));
     }
 
     /** @test */
