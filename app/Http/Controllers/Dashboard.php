69a713d4ed5fc8247731878a5312815824cc046d
wahdam <wahdam.amarulloh@gmail.com>
2021-12-24 13:35:05 +0700

    fix dashboard

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -9,30 +9,6 @@ class Dashboard extends Controller
 {
     public function main()
     {
-
-        $indicators = indicator()
-            ->symbol('ETH/USDT')
-            ->interval('15m')->get();
-
-        $pipe = app(Pipeline::class)
-            ->send($indicators)
-            ->via('buy')
-            ->through([
-                Rsi::class
-            ])
-            ->then(function ($indicators) {
-                return $indicators;
-            });
-
-        debug(collect([
-            'indicators' => 'all',
-        ]));
-
-
-        if ($pipe->get('should_buy')) {
-            debug($pipe->get('via'));
-        }
-
         return view('asset');
     }
 }
