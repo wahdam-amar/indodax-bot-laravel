79ff04b6a93cb235ae3834a1a1b4c6a197148511
wahdam <wahdam.amarulloh@gmail.com>
2022-01-03 22:46:07 +0700

    add setting relation

--- app/Models/Backtest/Backtest.php
+++ app/Models/Backtest/Backtest.php
@@ -3,6 +3,7 @@
 namespace App\Models\Backtest;
 
 use App\Models\User;
+use App\Models\UserSetting;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -25,5 +26,10 @@ class Backtest extends Model
     public function balance()
     {
         return $this->hasOne(BacktestBalance::class);
+    }
+
+    public function setting()
+    {
+        return $this->hasOneThrough(UserSetting::class, User::class, 'id', 'user_id', 'user_id', 'id');
     }
 }
